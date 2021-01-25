48e8ac6338a2f7c8c58ffad2165b6b85aee102b9
Patryk Piotrowski <piotrowski@patryk.org>
2021-01-25 19:38:28 +0100

    models.py and documents.py

---
+++ pypilogger/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+class Package(models.Model):
+    """ Package model - for keeping PyPI packages info """
+    title = models.CharField(_("title"), max_length=255)
+    link = models.CharField(_("link"), max_length=255)
+    guid = models.CharField(_("guid"), max_length=255)
+    description = models.TextField(_("description"))
+    author = models.CharField(_("author"), max_length=255)
+    author_email = models.CharField(_("author email"), max_length=255)
+    pub_date = models.DateTimeField(_("publication date"))
+    keywords = models.TextField(_("keywords"), null=True, blank=True)
+    version = models.CharField(_("version"), max_length=50)
+    # -- maintainer details --
+    maintainer = models.CharField(_("maintainer"), max_length=50, null=True, blank=True)
+    maintainer_email = models.CharField(_("mainteiner email"), max_length=50, null=True, blank=True)
+
+    def __str__(self):
+        return self.title
